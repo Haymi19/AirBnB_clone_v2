fcb0f7c97d217a59452b0657206010b7f5f15505
Haimanot Eshete <haimanoteshete685@gmail.com>
2023-05-03 12:34:12 -0700

    web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,13 +3,8 @@
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
-
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
 sudo echo "<html>
   <head>
   </head>
@@ -17,11 +12,7 @@ sudo echo "<html>
     Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
-
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
-
 sudo chown -R ubuntu:ubuntu /data/
-
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
-
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 sudo service nginx restart
