baa8a55bf0d86032946de08b075f3a159d2fa2e1
Haimanot Eshete <haimanoteshete685@gmail.com>
2023-05-03 12:48:59 -0700

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,5 +14,5 @@ sudo echo "<html>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/listen 80 default_server/https://haymi.tech/hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 sudo service nginx restart
